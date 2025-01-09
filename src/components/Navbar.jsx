d5d302596d83f5240824a3a23b9e6f878d75cbdf
Panxitus <gonzalezuribe.francisco@gmail.com>
2025-01-09 00:30:03 -0300

    navbar ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../../src/assets/images/logo.png';
 import { FaBars } from "react-icons/fa";
-const Navbar = ()=>{
+const Navbar = () =>{
   return (
-    <nav>
-        <div className="container nav__container">
-            <Link to='/'><img src={logo} alt='Hospital Logo'/></Link>
-            <ul className="nav__links">
+    <nav className="nav">
+        <div className="nav__container">
+            <Link to='/' className='nav__logo'><img src={logo} 
+              alt='nav Logo'/></Link>
+            <ul className="nav__menu">
                 <li><NavLink to='/about'>Sobre Nosotros</NavLink></li>
                 <li><NavLink to='/contact'>Contacto</NavLink></li>
                 <li><NavLink to='/doctores'>Doctores</NavLink></li>
                 <li><NavLink to='/Gallery'>Galeria</NavLink></li>
             </ul>
-           <button className="nav_toggle-btn"> 
+           <button className="nav__toggle-btn"> 
                 <FaBars/>
            </button>
         </div>
-
     </nav>
   )
 }
